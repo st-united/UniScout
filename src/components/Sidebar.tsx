6d98ef74318601b68f644c1f39aea43bcaf27522
bxthien <buixuanthien.0105@gmail.com>
2025-07-03 21:16:05 +0700

    feat: update dashboard route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -24,7 +24,7 @@ const Sidebar: React.FC<SidebarProps> = ({ activeTab, setActiveTab }) => {
       id: 'dashboard',
       label: 'Dashboard',
       icon: LayoutDashboard,
-      path: '',
+      path: '/',
     },
     {
       id: 'manage-university',
